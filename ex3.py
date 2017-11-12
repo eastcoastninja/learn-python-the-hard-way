2d550c68761d10fe35b95b005363906ecd04b1e0
Julian Rios <julian1rios@gmail.com>
2017-11-11 19:41:38 -0500

    Exercise 3: Numbers and Math

---
+++ ex3.py
@@ -0,0 +1,46 @@
+print("I will now count my chickens:")
+
+print("Hens", 25 + 30 / 6) # Returns 30.0 = 25 + 5.0 a double because of the division
+print("Roosters", 100 - 25 * 3 % 4) # Returns 97 = 100 - (25 * 3 = 75) (75 % 4 = 3) --> 100 - 3 = 97
+
+print("Now I will count the eggs:") # Returns a string
+
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6) # Returns 6.75 = (3 + 2 + 1 = 6) - 5 + (4 % 2 = 0) - (1 / 4 = .25) + 6 --> 6 + 6 - 5 - .25 = 6.75
+
+print("Is it true that 3 + 2 < 5 - 7?") # Returns a string
+
+print(3 + 2 < 5 - 7) # 5 < -2 Returns a boolean value true or false --> This is FALSE
+
+print("What is 3 + 2?", 3 + 2) # Returns 5
+print("What is 5 - 7?", 5 - 7) # Returns -2
+
+print("Oh, that's why it's False.") # Returns a string
+
+print("How about some more.") # Returns a string
+
+print("Is it greater?", 5 > -2) # Returns a boolean True
+print("Is it greater or equal?", 5 >= -2) # Returns a boolean True
+print("Is it less than or equal?", 5 <= -2) # Returns a boolean False
+
+print("# <---------CODE BELOW RETURNS FLOATING VALUES--------------->")
+print("Hens", 25 + 30 / 6.0) # Returns 30.0 = 25 + 5.0 a double because of the division
+print("Roosters", 100 - 25 * 3 % 4.0) # Returns 97.0 = 100 - (25 * 3 = 75) (75 % 4 = 3) --> 100 - 3 = 97
+
+print("Now I will count the eggs:") # Returns a string
+
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6.0) # Returns 6.75 = (3 + 2 + 1 = 6) - 5 + (4 % 2 = 0) - (1 / 4 = .25) + 6 --> 6 + 6 - 5 - .25 = 6.75
+
+print("Is it true that 3 + 2 < 5 - 7?") # Returns a string
+
+print(3 + 2 < 5 - 7.0) # 5 < -2 Returns a boolean value true or false --> This is FALSE
+
+print("What is 3 + 2?", 3 + 2.0) # Returns 5.0
+print("What is 5 - 7?", 5 - 7.0) # Returns -2.0
+
+print("Oh, that's why it's False.") # Returns a string
+
+print("How about some more.") # Returns a string
+
+print("Is it greater?", 5 > -2.0) # Returns a boolean True
+print("Is it greater or equal?", 5 >= -2.0) # Returns a boolean True
+print("Is it less than or equal?", 5 <= -2.0) # Returns a boolean False
---
+++ ex3Calc.py
@@ -0,0 +1,4 @@
+import math
+
+print("Let's calculate the the square root of 400!")
+print("The value is:", math.sqrt(400))
